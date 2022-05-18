5b90bea34a86fcf1398fe4f5229fa243c89a5161
MichaelDViau <michael.delagrave.viau@gmail.com>
2022-05-18 16:23:59 -0500

    prefinal coming soon>

--- assets/js/slider-bg.js
+++ assets/js/slider-bg.js
@@ -1,17 +1,1 @@
- $(document).ready(function() {
- if ($('[data-background]').length > 0) {
-      $('[data-background]').each(function() {
-        var $background, $backgroundmobile, $this;
-        $this = $(this);
-        $background = $(this).attr('data-background');
-        $backgroundmobile = $(this).attr('data-background-mobile');
-        if ($this.attr('data-background').substr(0, 1) === '#') {
-          return $this.css('background-color', $background);
-        } else if ($this.attr('data-background-mobile') && device.mobile()) {
-          return $this.css('background-image', 'url(' + $backgroundmobile + ')');
-        } else {
-          return $this.css('background-image', 'url(' + $background + ')');
-        }
-      });
-    }
-  });
+$(document).ready(function(){0<$("[data-background]").length&&$("[data-background]").each(function(){var a=$(this),t=$(this).attr("data-background"),r=$(this).attr("data-background-mobile");return"#"===a.attr("data-background").substr(0,1)?a.css("background-color",t):a.attr("data-background-mobile")&&device.mobile()?a.css("background-image","url("+r+")"):a.css("background-image","url("+t+")")})});
